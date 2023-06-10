80b95b2eb28a45d2805c183b0662634b770e9d66
bhavani_p <bhavaniputtaraj12@gmail.com>
2023-06-10 21:50:14 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { Firestore,
+collection,
+addDoc,
+collectionData,
+doc,
+updateDoc,
+deleteDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-fire';
-}
+  userData!: Observable<any>;
+
+
+  constructor(private firestore: Firestore){
+    this.getData();
+  }
+
+  addData(f : any){
+   const collectionInstance = collection(this.firestore, 'users');
+   addDoc(collectionInstance, f.value)
+   .then(() => {
+    console.log('Data Saved Successfully');
+   })
+   .catch((err)=> {
+    console.log(err);
+   })
+
+   }
+   getData(){
+    const collectionInstance = collection(this.firestore, 'users');
+    collectionData(collectionInstance)
+    .subscribe(val => {
+      console.log(val);
+    })
+    this.userData = collectionData(collectionInstance, { idField: 'id'});
+   }
+   updateData(id:string){
+    const docInstance = doc(this.firestore, 'users',id);
+    const updateData = {
+      name: 'updateName'
+    }
+    updateDoc(docInstance, updateData)
+    .then(() => {
+      console.log('Data Updated');
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+
+   }
+   deleteData(id : string){
+    const docInstance = doc(this.firestore, 'users',id);
+    deleteDoc(docInstance)
+    .then(() => {
+      console.log('Data Deleted');
+    })
+
+   }
+  }
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-fire-e9bb5',
+    appId: '1:832695373223:web:c53da35dcb929748e1ba82',
+    storageBucket: 'angular-fire-e9bb5.appspot.com',
+    apiKey: 'AIzaSyAm-CVrbSKVx7xFkPArsi4RYSO2YE1o66Q',
+    authDomain: 'angular-fire-e9bb5.firebaseapp.com',
+    messagingSenderId: '832695373223',
+  },
+};
